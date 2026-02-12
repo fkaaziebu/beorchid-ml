26ae2ee7cf305efc44074ab2d9ce5576341faac5
fkaaziebu <frederickaziebu1998@gmail.com>
2026-02-12 17:17:08 +0000

    remove dummy non-ml endpoints

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,8 +3,6 @@ from fastapi import APIRouter
 from app.api.v1.endpoints import (
     disease_prediction,
     health,
-    items,
-    users,
     weed_growth_prediction,
     yield_prediction,
 )
@@ -12,8 +10,8 @@ from app.api.v1.endpoints import (
 api_router = APIRouter()
 
 api_router.include_router(health.router, tags=["health"])
-api_router.include_router(users.router)
-api_router.include_router(items.router)
+# api_router.include_router(users.router)
+# api_router.include_router(items.router)
 api_router.include_router(disease_prediction.router)
 api_router.include_router(weed_growth_prediction.router)
 api_router.include_router(yield_prediction.router)
